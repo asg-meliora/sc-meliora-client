67724581aed9188ce2e70a82e25ca72b51c4bd33
asg-meliora <alejandro.suarez@meliora.mx>
2025-04-11 02:02:45 -0600

    wip: Files Table Design finish

--- src/components/Files/FilesTable.jsx
+++ src/components/Files/FilesTable.jsx
@@ -1,63 +1,130 @@
-import React from 'react'
+import React from "react";
 import "./FilesTable.css";
 import { useNavigate } from "react-router-dom";
+import styles from "../../styles";
+import { SlOptionsVertical } from "react-icons/sl";
 
 function FilesTable({ api, newFiles }) {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    // "client_id": 6,
-    // "name_rs": "exampleRS5",
-    // "rfc": "GFC435",
-    // "curp": "LPKI2RTY5",
-    // "address": "calle5",
-    // "zip_code": "904504",
-    // "phone": "7771232334",
-    // "email": "exapmle2cliente@ex.com",
-    // "bank_account": "0129301923"
-    // "created_at": "2025-03-25T00:57:41.000Z"
+  // "client_id": 6,
+  // "name_rs": "exampleRS5",
+  // "rfc": "GFC435",
+  // "curp": "LPKI2RTY5",
+  // "address": "calle5",
+  // "zip_code": "904504",
+  // "phone": "7771232334",
+  // "email": "exapmle2cliente@ex.com",
+  // "bank_account": "0129301923"
+  // "created_at": "2025-03-25T00:57:41.000Z"
 
-    const FormattedDate = ({ dateString }) => {
-        const date = new Date(dateString);
-        const formattedDate = `${date.getDate().toString().padStart(2, '0')}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getFullYear()}`;
-
-        return <div>{formattedDate}</div>;
-    };
-
-    //console.log('Debug Table',newFiles);
-    return (
-        <table className="cont min-w-full">
-            <thead className="navbar">
-                <tr>
-                    <th className="px-4 py-2">Razón Social</th>
-                    <th className="px-4 py-2">RFC</th>
-                    <th className="px-4 py-2">CURP</th>
-                    <th className="px-4 py-2">Dirección</th>
-                    {/* <th className="px-4 py-2">C.P.</th> */}
-                    <th className="px-4 py-2">Teléfono</th>
-                    <th className="px-4 py-2">Email</th>
-                    <th className="px-4 py-2">Número de cuenta</th>
-                    <th className="px-4 py-2">Fecha de creación</th>
-                    {/* <th className="px-4 py-2">ACCIONES</th> */}
-                </tr>
-            </thead>
-            <tbody>
-                {newFiles.results.map((results, index) => (
-                    <tr className="navbar cursor-pointer hover:bg-green-600" key={results.client_id || results.insertId} onClick={ ()=>navigate(`/files/details/${results.client_id}`) }>
-                        <td className="px-4 py-2">{results.name_rs}</td>
-                        <td className="px-4 py-2">{results.rfc}</td>
-                        <td className="px-4 py-2">{results.curp}</td>
-                        <td className="px-4 py-2">{results.address}</td>
-                        {/* <td className="px-4 py-2">{results.zip_code}</td> */}
-                        <td className="px-4 py-2">{results.phone}</td>
-                        <td className="px-4 py-2">{results.email}</td>
-                        <td className="px-4 py-2">{results.bank_account}</td>
-                        <td className="px-4 py-2"><FormattedDate dateString={results.created_at}/></td>
-                        {/* <td className="px-4 py-2 bg-blue-500 hover:bg-green-600 rounded-[12px]"> <button className= "text-white" onClick={ ()=>navigate('/') }>PLAY</button> </td> */}
-                    </tr>
-                ))}
-            </tbody>
-        </table>
+  const FormattedDate = ({ dateString }) => {
+    const date = new Date(dateString);
+    const formattedDate = `${date.getDate().toString().padStart(2, "0")}/${(
+      date.getMonth() + 1
     )
+      .toString()
+      .padStart(2, "0")}/${date.getFullYear()}`;
+
+    return <div>{formattedDate}</div>;
+  };
+
+  //console.log('Debug Table',newFiles);
+  return (
+    <>
+      <div className={styles.table_layout}>
+        <div className={styles.table_container}>
+          <table className={`${styles.table} rounded-2xl`}>
+            <thead className={styles.table_header}>
+              <tr>
+                {[
+                  "Razón Social",
+                  "RFC",
+                  "CURP",
+                  "Dirección",
+                  "Teléfono",
+                  "Email",
+                  "Número de cuenta",
+                  "Fecha de creación",
+                  "Acciones",
+                ].map((title) => (
+                  <th key={title} className={styles.table_header_cell}>
+                    {title}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody className={styles.table_body}>
+              {newFiles.results.map((item, index) => (
+                <tr
+                  key={item.client_id || item.insertId}
+                  onClick={() => navigate(`/files/details/${item.client_id}`)}
+                  className={`border-b-[2.5px] border-[#b9b9b9]  last:border-none ${
+                    index % 2 === 0 ? "bg-gray-50" : "bg-[#c5c5c5]"
+                  } hover:bg-[#313131] hover:cursor-pointer hover:text-white transition-all`}
+                >
+                  <td className="p-4 text-center font-semibold">
+                    {item.name_rs}
+                  </td>
+                  <td className="p-4 text-center">{item.rfc}</td>
+                  <td className="p-4 text-center">{item.curp}</td>
+                  <td className="p-4 text-center">{item.address}</td>
+                  <td className="p-4 text-center">{item.phone}</td>
+                  <td className="p-4 text-center">{item.email}</td>
+                  {/* <td className="p-4 text-center">{item.zip_code}</td> */}
+                  <td className="p-4 text-center">{item.bank_account}</td>{" "}
+                  <td className="p-4 text-center">
+                    <FormattedDate dateString={item.created_at} />
+                  </td>
+                  <td className="p-4 text-center">
+                    <button
+                      onClick={() => {}}
+                      className="text-[#9e824f] hover:text-[#eeb13f] pr-1 pl-2  hover:cursor-pointer transition-all transform hover:scale-120"
+                    >
+                      <SlOptionsVertical size={18} />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+    // ! --------------------------------------------------------------------- \\
+    // <table className="cont min-w-full">
+    //     <thead className="navbar">
+    //         <tr>
+    //             <th className="px-4 py-2">Razón Social</th>
+    //             <th className="px-4 py-2">RFC</th>
+    //             <th className="px-4 py-2">CURP</th>
+    //             <th className="px-4 py-2">Dirección</th>
+    //             {/* <th className="px-4 py-2">C.P.</th> */}
+    //             <th className="px-4 py-2">Teléfono</th>
+    //             <th className="px-4 py-2">Email</th>
+    //             <th className="px-4 py-2">Número de cuenta</th>
+    //             <th className="px-4 py-2">Fecha de creación</th>
+    //             {/* <th className="px-4 py-2">ACCIONES</th> */}
+    //         </tr>
+    //     </thead>
+    //     <tbody>
+    //         {newFiles.results.map((results, index) => (
+    //             <tr className="navbar cursor-pointer hover:bg-green-600" key={results.client_id || results.insertId} onClick={ ()=>navigate(`/files/details/${results.client_id}`) }>
+    //                 <td className="px-4 py-2">{results.name_rs}</td>
+    //                 <td className="px-4 py-2">{results.rfc}</td>
+    //                 <td className="px-4 py-2">{results.curp}</td>
+    //                 <td className="px-4 py-2">{results.address}</td>
+    //                 {/* <td className="px-4 py-2">{results.zip_code}</td> */}
+    //                 <td className="px-4 py-2">{results.phone}</td>
+    //                 <td className="px-4 py-2">{results.email}</td>
+    //                 <td className="px-4 py-2">{results.bank_account}</td>
+    //                 <td className="px-4 py-2"><FormattedDate dateString={results.created_at}/></td>
+    //                 {/* <td className="px-4 py-2 bg-blue-500 hover:bg-green-600 rounded-[12px]"> <button className= "text-white" onClick={ ()=>navigate('/') }>PLAY</button> </td> */}
+    //             </tr>
+    //         ))}
+    //     </tbody>
+    // </table>
+  );
 }
 
-export default FilesTable
+export default FilesTable;
