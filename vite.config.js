e7571daf88ea4f2b15b180bf8901f043ea3bc776
asg-meliora <alejandro.suarez@meliora.mx>
2025-05-09 13:50:03 -0600

    fix: base route refix

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "http://asg-meliora.github.io/sc-meliora-client"
+  base: "https://asg-meliora.github.io/sc-meliora-client"
 })
